5a0102c00fe3deee15f6b5f22e29ef339bd9653a
wes137 <sebas.a.nunez@gmail.com>
2020-05-12 18:22:38 -0400

    Updates

---
+++ precache-manifest.b5d2e904f9ac26cbd48e5f362a4d9774.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4bfd795396c635155869701c2fa862",
+    "url": "/realtime-chat/index.html"
+  },
+  {
+    "revision": "201b0560ca41d7ab1cba",
+    "url": "/realtime-chat/static/css/2.0a761415.chunk.css"
+  },
+  {
+    "revision": "5c2b10e516d6990d6ff7",
+    "url": "/realtime-chat/static/css/main.140e3c95.chunk.css"
+  },
+  {
+    "revision": "201b0560ca41d7ab1cba",
+    "url": "/realtime-chat/static/js/2.0c00ce4f.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/realtime-chat/static/js/2.0c00ce4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2b10e516d6990d6ff7",
+    "url": "/realtime-chat/static/js/main.f3e53a20.chunk.js"
+  },
+  {
+    "revision": "d532585ea261110ba7b7",
+    "url": "/realtime-chat/static/js/runtime-main.e7cc639b.js"
+  },
+  {
+    "revision": "492b5f12ccf366f67b6fca5ab64e4600",
+    "url": "/realtime-chat/static/media/clip.492b5f12.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/realtime-chat/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/realtime-chat/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/realtime-chat/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/realtime-chat/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/realtime-chat/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "75618e6737997759b02e3e471eb60d7a",
+    "url": "/realtime-chat/static/media/menu.75618e67.svg"
+  },
+  {
+    "revision": "842988758d7b29dd17e1bcb0ff3edaa7",
+    "url": "/realtime-chat/static/media/messages.84298875.svg"
+  },
+  {
+    "revision": "3929db036c2492150e2bea9d84c27e98",
+    "url": "/realtime-chat/static/media/microphone.3929db03.svg"
+  },
+  {
+    "revision": "5b695a54d6e78050f65203603572df3c",
+    "url": "/realtime-chat/static/media/search.5b695a54.svg"
+  }
+]);
